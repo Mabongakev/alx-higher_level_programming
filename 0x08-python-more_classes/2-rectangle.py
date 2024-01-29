9e625fbcba7dc6f3eda3a9d4f38962823094e8e6
Mabongakev <kelvin.mabonga92@gmail.com>
2024-01-29 08:43:32 -0800

    get area and perimeter of the rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -42,11 +42,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """return the area of the rectangle"""
+        """Return the area of the Rectangle."""
         return (self.__width * self.__height)
 
     def perimeter(self):
-        """return the perimeter of the rectangle"""
+        """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
-        return ((self.__width * 2) + (self.__width * 2))
+        return ((self.__width * 2) + (self.__height * 2))
