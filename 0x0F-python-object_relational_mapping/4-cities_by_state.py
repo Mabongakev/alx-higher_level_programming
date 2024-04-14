1c7cbd99ab2c00c118c6b63afe9011c32b1905df
Mabongakev <kelvin.mabonga92@gmail.com>
2024-04-14 04:02:57 -0700

    lists all cities but executed once

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,11 +4,9 @@ import MySQLdb
 import sys
 
 
-
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], password=sys.argv[2], db=sys.argv[3], port=3306)
     curs = db.cursor()
-    match_arg = sys.argv[4]
     curs.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id=cities.state_id")
     rows = curs.fetchall()
     for row in rows:
