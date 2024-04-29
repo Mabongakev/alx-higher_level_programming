66b70f02342e637a73a53c4fbf5d145bb2fe88d9
Mabongakev <kelvin.mabonga92@gmail.com>
2024-04-29 22:31:25 +0300

    Display the size of the body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if the user provided a URL argument
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-# Send a GET request to the URL and store the response body
-echo $(curl -sI "$1" | grep "Content-Length" | cut -d ":" -f 2)
+#Display the size of the body
+echo $(curl -sI "$1" | grep 'Content-Length' | cut -d ':' -f 2)
